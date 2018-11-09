113cbfa570b68851786b22b831640f93fbc11616
tiana-horn <tiana.horn@gmail.com>
2018-11-09 13:47:38 -0500

    List of tests that need to be done

---
+++ test_pathfinder.py
@@ -0,0 +1,6 @@
+"""
+Test that program can make a matrix of points
+Test that can find the touching coordinates based on given coordinate
+Test that can find the coordinates with lowest abs value based on given coordinate
+Test that you can update the current position of coordinate
+"""
