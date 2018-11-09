b2115832f4b32684b447ff6042aedc67344d858d
tiana-horn <tiana.horn@gmail.com>
2018-11-09 15:09:06 -0500

    List of list working

--- pathfinder.py
+++ pathfinder.py
@@ -1,44 +1,72 @@
 from PIL import Image
+ 
+w = 5
+h = 3
+
+# class Read:
+def read_file():
+    with open("elevation_small.txt") as data:
+        for line in data:
+            Matrix = [int(num) for num in line.split()]
+            # for line in data.readlines():
+            # elevation_map.append(Matrix)
+    #     elevation_map.append(line)
+    #     w = len(line)
+    #     h = len(line)
+    # for i in elevation_map:
+    #     Matrix = [[i for x in range(w)] for y in range(h)]
+    #     print(Matrix)
+        # Matrix.image.putpixel(x,y)(255, 0, 0)
+       # print(Matrix)
+    #    print(elevation_map[5:])
+            print(Matrix)
+
+read_file()
+
+# 
 
 
-class Read:
-    """First read the elevation file
-    Then turn the file into a list"""
 
 
 
 
-class Map:
-    """
-    Turn each number is elevation list into coordinates with a color for each coordinate
-    Then transform those color coordinates to grayscale
-    """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
-    def save(self, filename):
-        self.image.save(filename)
+# class Read:
+#     """First read the elevation file
+#     Then turn the file into a list of lists"""
+#     elevation_map = []
+#     with open("elevation_small.txt") as data:
+#             for line in data.readlines():
+#                 elevation_map.append(line) 
 
-    def draw_point(self, coords, color):
-        self.image.putpixel(coords, color)
 
-    def map_from_file(self,filename):
-        elevation_map = [[]]
-        elevation_test = [[]]
-        with open("elevation_small.txt") as data:
-            for line in data.readlines():
-                elevation_map.append(line) 
-                for y, row in enumerate(elevation_map):
-                    for x, num in enumerate(row):
-                        elevation_test = image.putpixel((x,y), (num, num, num))
-                        map.save("elevation.png")
-                        open "elevation.png"
+# class Map:
+#     """
+#     Turn each number is elevation list into coordinates with a color for each coordinate
+#     Then transform those color coordinates to grayscale
+#     """
+#     def __init__(self, w, h):
+#         self.w = w
+#         self.h = h
 
-class Chart:
-    """first, find the lowest coordinate on the left. 
-    Then find the coordinates to the right that are touching. 
-    Then find the coordinate with the smallest absolute value.""" 
+#     def save(self, filename):
+#         self.image.save(filename)
+
+#     def draw_point(self, coords, color):
+#         self.image.putpixel(coords, color)
+
+#     def map_from_file(self,filename):
+#         elevation_test = []
+#         for y, row in enumerate(elevation_map):
+#                 for x, num in enumerate(row):
+#                     elevation_test = image.putpixel((x,y), (num, num, num))
+#                     map.save("elevation.png")
+                       
+
+# class Chart:
+#     """first, find the lowest coordinate on the left. 
+#     Then find the coordinates to the right that are touching. 
+#     Then find the coordinate with the smallest absolute value.""" 
 
                        
                 # elevation_test.append([num])
