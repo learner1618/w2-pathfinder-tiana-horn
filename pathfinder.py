d91ae63a624c1725b8f0a6f4e11635958c897575
tiana-horn <tiana.horn@gmail.com>
2018-11-12 21:24:37 -0500

    Map printing, max elevation initialized

--- pathfinder.py
+++ pathfinder.py
@@ -30,14 +30,13 @@ class Map:
     Turn each number is elevation list into coordinates with a color for each coordinate
     Then transform those color coordinates to grayscale
     """
-    def __init__(self, width, height, highest_point):
+    def __init__(self, width, height):
         self.width = width
         self.height = height
         self.image = Image.new("RGB", (width, height))
-        self.highest_point = highest_point
-       
         with open("elevation_small.txt") as data:
-            self.matrix = [[int(x) for x in line.split()] for line in data]        
+            self.matrix = [[int(x) for x in line.split()] for line in data] 
+        self.maxelevation = max([max(n) for n in self.matrix])       
 
     def save(self, filename):
         self.image.save(filename)
@@ -45,20 +44,13 @@ class Map:
     def draw_point(self, coords, color):
         self.image.putpixel(coords, color)
 
-    def find_highest_elevation(self):
-       self.highest_point = max([max(n) for n in self.matrix])
-       return self.highest_point
-
     def map_from_file(self,filename):
             for y, row in enumerate(self.matrix): 
                 for x, num in enumerate(row):
-                    pos = int((num/self.highest_point)*255)
+                    pos = int((num/self.maxelevation)*255)
                     self.image.putpixel((x,y), (pos, pos, pos))
             return self
 
-    # def find_highest_elevation(self,): 
-    #     map_from_file()
-    #     max_point = max(matrix)
 
 # class Chart:
 #     """first, find the lowest coordinate on the left. 
@@ -77,8 +69,8 @@ class Map:
 
 if __name__=="__main__":
     
-    my_map = Map(600,600, 5648) 
+    my_map = Map(600,600) 
     my_map = my_map.map_from_file("elevation_small.txt")
-    my_map.find_highest_elevation()
+    # my_map.find_highest_elevation()
     my_map.save("elevation.png")
     my_map.image.show("elevation.png")
